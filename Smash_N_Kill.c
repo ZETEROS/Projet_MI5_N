1ef52d5f4cba588cb16f4ce3c5594b7deb0ebac0
ZETEROS <92261313+ZETEROS@users.noreply.github.com>
2025-04-17 14:41:34 +0200

    Add files via upload
    17/04/2025 Version . Now this version has working buttons.

--- Smash_N_Kill.c
+++ Smash_N_Kill.c
@@ -1,7 +1,10 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+#include "lib/buttons-coord.h"
 
 //Dimensions
 #define WIDTH 1280
@@ -31,13 +34,17 @@ int main(){
         printf("Error, unable to start SDL_Image\n%s",IMG_GetError());
         exit(2);
     }
+    if ( Mix_OpenAudio(44100 , MIX_DEFAULT_FORMAT , 2 , 2048) < 0){
+        printf("Error , couldnt open the mixer.\n%s",Mix_GetError());
+        exit(3);
+    }
 
     //Create Window
 
     SDL_Window* Game = SDL_CreateWindow("Smash&Kill", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT , SDL_WINDOW_SHOWN);
     if (!Game){
         printf("Error, unabler to create the window.\n");
-        exit(3);
+        exit(4);
     }
 
     //Create Renderer
@@ -45,10 +52,9 @@ int main(){
     SDL_Renderer* render = SDL_CreateRenderer(Game , -1 , SDL_RENDERER_ACCELERATED);
     if(!render){
         printf("Error , unable to create renderer.\n");
-        exit(4);
+        exit(5);
     }
 
-    SDL_Texture* menu = loadTexture("assets/Menu.png", render);
 
     //GAME RUNNING
     int quit = 0 ;
@@ -61,10 +67,14 @@ int main(){
         FIGHT,
         END_GAME,
         HOW_TO_PLAY,
-        EXIT
+        
     }Game_State;
 
     Game_State state = MENU ;
+
+    SDL_Texture* menu_screen = loadTexture("assets/Menu.png", render);
+    SDL_Texture* how_to_play_screen = loadTexture("assets/How_to_play.png", render);
+
 
     while(!quit){
         while(SDL_PollEvent(&event)){
@@ -77,12 +87,35 @@ int main(){
 
                 case MENU :
                     SDL_RenderClear(render);
-                    SDL_RenderCopy(render , menu , NULL , NULL);
+                    SDL_RenderCopy(render , menu_screen , NULL , NULL);
                     SDL_RenderPresent(render);
+                    //Clicked?
+                    if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT){
+                        int x = event.button.x ;
+                        int y = event.button.y ;
+
+                        //WHERE?
+                        if( x >= Button_PLAY.x && x <= Button_PLAY.x + Button_PLAY.w && y >= Button_PLAY.y && y <= Button_PLAY.y + Button_PLAY.h){
+                            //Go to the next part , the GAME MODE SELECTION SCREEN.
+                            state = GAMEMODE_SELECTION;
+                        }
+                        else if( x >= Button_HOWTOPLAY.x && x <= Button_HOWTOPLAY.x + Button_HOWTOPLAY.w && y >= Button_HOWTOPLAY.y && y <= Button_HOWTOPLAY.y + Button_HOWTOPLAY.h){
+                            //Go to the next part , the GAME MODE SELECTION SCREEN.
+                            state = HOW_TO_PLAY;
+                        }
+                        else if( x >= Button_EXIT.x && x <= Button_EXIT.x + Button_EXIT.w && y >= Button_EXIT.y && y <= Button_EXIT.y + Button_EXIT.h){
+                            //Exit the Game.
+                            quit = -1 ;
+                        }
+
+
+                    }
+
+
                     break;
                 
                 case GAMEMODE_SELECTION :
-
+                    quit = -1; //Currently only exiting the game.
                     break;
                 
                 case TEAM_SELECTION :
@@ -98,11 +131,9 @@ int main(){
                     break;
                 
                 case HOW_TO_PLAY:
-
-                    break;
-                
-                case EXIT:
-                    quit = 1;
+                    SDL_RenderClear(render);
+                    SDL_RenderCopy(render , how_to_play_screen , NULL , NULL);
+                    SDL_RenderPresent(render);
                     break;
 
             }
