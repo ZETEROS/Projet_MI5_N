ed213f139bc459dfefe40e3ec6246b2e364fce82
ZETEROS <92261313+ZETEROS@users.noreply.github.com>
2025-04-17 14:43:35 +0200

    Buttons Coordinates
    This is the file with the coordinates of the buttons. Necessary .

---
+++ lib/buttons-coord.h
@@ -0,0 +1,13 @@
+#ifndef BUTTONS_COORD_H
+#define BUTTONS_COORD_H
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+SDL_Rect Button_PLAY = {580 , 409 , 119 , 70};
+SDL_Rect Button_HOWTOPLAY = {459 , 508 , 361 , 70};
+SDL_Rect Button_EXIT = {583 , 610 , 119 , 70};
+
+#endif
