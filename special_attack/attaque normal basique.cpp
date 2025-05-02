93d6def42444b74c4622b257eff7411e32f2b591
stillnoss <noa.jubert@gmail.com>
2025-05-02 15:09:20 +0200

    Delete special_attack directory

--- special_attack/attaque normal basique.cpp
+++
@@ -1,4 +0,0 @@
-void attack(Fighter *target, Fighter *attacant){
-    float impact= (attacant->dmg * (100-(target->def)/100));
-    target->hp -= impact;
-}
--- special_attack/tackle.c
+++
@@ -1,8 +0,0 @@
-void tackle(Fighter *target, Fighter *attacant){
-    correct_def=target->def;
-    target->def*=0.70;
-    attack(Fighter *target, Fighter *attacant);
-    target->def=correct_def;
-}
-
-void (*special_move)(Fighter *, Fighter *) = tackle;
