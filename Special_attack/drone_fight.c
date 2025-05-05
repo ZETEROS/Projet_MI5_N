54f38caee1cd3ef95b3eb4d473802ae0e0b81fe8
IronApsol <59450787+IronApsol@users.noreply.github.com>
2025-05-05 02:38:51 +0200

    Update drone_fight.c

--- Special_attack/drone_fight.c
+++ Special_attack/drone_fight.c
@@ -1,8 +1,8 @@
 void drone_fight(Fighter* attacker, Fighter* defender, Fighter* ally){
     if(ally->hp>ally->hp_max-20){
-        ally->hp=ally->hp_max
+        ally->hp=ally->hp_max;
     }else{
-        ally->hp+=20
+        ally->hp+=20;
     }
     // importe le fighter et redonner les stats par défat pour esuqie/attack/defense
 }
