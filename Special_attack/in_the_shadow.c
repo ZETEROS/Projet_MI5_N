627a33b814f29074e26a2f31186313dccf7f50ef
IronApsol <59450787+IronApsol@users.noreply.github.com>
2025-05-05 02:39:07 +0200

    Update in_the_shadow.c

--- Special_attack/in_the_shadow.c
+++ Special_attack/in_the_shadow.c
@@ -1,4 +1,4 @@
 void in_the_shadow(Fighter* attacker, Fighter* defender){
     attack(Fighter *target, Fighter *attacant)
-    attacker->dodge=100 // trouver un moyen de durée de 1 tour
+    attacker->dodge=100; // trouver un moyen de durée de 1 tour
 }
