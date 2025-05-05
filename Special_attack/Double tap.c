2a8c9af31bc6cb530c00222ec3a6d4a95fc59c91
IronApsol <59450787+IronApsol@users.noreply.github.com>
2025-05-05 02:38:09 +0200

    Update Double tap.c

--- Special_attack/Double tap.c
+++ Special_attack/Double tap.c
@@ -3,7 +3,7 @@ void Double_tap(Fighter *target, Fighter *attacant){
     attacant->dmg = attacant->dmg*0.75;
     attack(Fighter *target, Fighter *attacant);
     attack(Fighter *target, Fighter *attacant);
-    attacant->dmg = correct_dmg
+    attacant->dmg = correct_dmg;
 }
 
 // appelation : special_move(target, attacant);
