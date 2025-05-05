0ad6096d9886b48739ac1a3e21251cfcfb4620a3
IronApsol <59450787+IronApsol@users.noreply.github.com>
2025-05-05 02:38:42 +0200

    Update drainage.c

--- Special_attack/drainage.c
+++ Special_attack/drainage.c
@@ -5,10 +5,8 @@ void drainage(Fighter *target, Fighter *attacant){
     target->def=correct_def;
     float hp_drained=attacant->dmg * 0.40
     if(attacant->hp + hp_drained > hp_max){
-        attacant->hp=hp_max
+        attacant->hp=hp_max;
     }else{
-        attacant->hp+= hp_drained
+        attacant->hp+= hp_drained;
     }
 }
-
-void (*special_move)(Fighter *, Fighter *) = drainage;
