a9b6dd54fb1c0532def9a971759e703313e86810
stillnoss <noa.jubert@gmail.com>
2025-05-08 22:56:20 +0200

    Update special_attacks_list.h

--- lib/special_attacks_list.h
+++ lib/special_attacks_list.h
@@ -28,30 +28,29 @@ void assign_special(Fighter* a){
 
 void special_attack(Fighter* Attacker, Fighter* Target) {
     if (strcmp(Attacker->name, "John_Wick") == 0) {
-        int damage = (int)(Attacker->attack * 0.75);  // 75% damage per shot
-        Target->hp -= 2 * damage;
+        int dmg = (int)(Attacker->dmg * 0.75);  // 75% damage per shot
+        Target->hp -= 2 * dmg;
     } else if (strcmp(Attacker->name, "Hulk") == 0) {
-        Target->attack = (int)(Target->attack * 0.8);  // Reduces attack by 20%
+        Target->dmg = (int)(Target->dmg * 0.8);  // Reduces dmg by 20%
     } else if (strcmp(Attacker->name, "Lifeline") == 0) {
         Attacker->hp = (Attacker->hp + 25 > 100) ? 100 : Attacker->hp + 25;
     } else if (strcmp(Attacker->name, "Sans") == 0) {
         Target->dodge /= 2;  // Reduces dodge by half
     } else if (strcmp(Attacker->name, "Ronflex") == 0) {
-        Target->defense = (int)(Target->defense * 0.7);  // Reduces defense by 30%
+        Target->def = (int)(Target->def * 0.7);  // Reduces def by 30%
     } else if (strcmp(Attacker->name, "Dracula") == 0) {
-        int heal = (int)(Attacker->attack * 0.4);  // Heal 40% of damage dealt
+        int heal = (int)(Attacker->dmg * 0.4);  // Heal 40% of dmg dealt
         Target->hp -= heal;
         Attacker->hp = (Attacker->hp + heal > 100) ? 100 : Attacker->hp + heal;
     } else if (strcmp(Attacker->name, "Batman") == 0) {
         // Batman becomes invisible for 1 turn
     } else if (strcmp(Attacker->name, "Demolisher") == 0) {
-        int damage = Attacker->attack;
-        Target->hp -= damage;
+        int dmg = Attacker->dmg;
+        Target->hp -= dmg;
     } else if (strcmp(Attacker->name, "Medic") == 0) {
         Attacker->hp = (Attacker->hp + 30 > 100) ? 100 : Attacker->hp + 30;
     }
 }
-
 //Fonction to import the fighter and to use the assign fonction saw before.
 Fighter importandassign(char* file , SDL_Texture** visuals , SDL_Texture** visuals_selected){
     Fighter a;
