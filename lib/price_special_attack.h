f8d5fe3960424b5f4f79028aec9d7245c4a18d4e
stillnoss <noa.jubert@gmail.com>
2025-05-11 15:37:34 +0200

    Add files via upload

--- lib/price_special_attack.h
+++ lib/price_special_attack.h
@@ -1,9 +1,11 @@
-price_touble_tap=90
-price_thunder_clap=15
-price_drone_heal=20
-price_immobilization=35
-price_tackle=75
-price_drainage=100
-price_in_the_shadows=50
-price_rock_throw=80
-price_crusaders_crossbow=35
+#include <stdio.h>
+
+int price_touble_tap = 90;
+int price_thunder_clap = 15;
+int price_drone_heal = 20;
+int price_immobilization = 35;
+int price_tackle = 75;
+int price_drainage = 100;
+int price_in_the_shadows = 50;
+int price_rock_throw = 80;
+int price_crusaders_crossbow = 35;
