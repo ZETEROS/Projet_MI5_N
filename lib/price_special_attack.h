4c3a54dd50d7b9705e62a0910ce7ab6f72a7cd3a
stillnoss <noa.jubert@gmail.com>
2025-05-11 15:39:23 +0200

    Update price_special_attack.h

--- lib/price_special_attack.h
+++ lib/price_special_attack.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int price_touble_tap = 90;
-int price_thunder_clap = 15;
-int price_drone_heal = 20;
+int price_double_tap = 90;
 int price_immobilization = 35;
-int price_tackle = 75;
-int price_drainage = 100;
 int price_in_the_shadows = 50;
+int price_thunder_clap = 15;
+int price_tackle = 75;
 int price_rock_throw = 80;
+int price_drone_heal = 20;
+int price_drainage = 100;
 int price_crusaders_crossbow = 35;
